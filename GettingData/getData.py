a3ac45c8f7eef6bb8e6e81acdfc1175822041ae5
Scott-Rosoff <scrosoff@gmail.com>
2020-11-10 21:57:53 -0800

    venv set up

---
+++ GettingData/getData.py
@@ -0,0 +1,4 @@
+import pandas as pd 
+df = pd.read_csv(r'C:\Users\rosof\Downloads\pbp-2019.csv')
+print(df.head())
+print('test')
---
+++ TODO.md
@@ -0,0 +1,3 @@
+# To do:
+1) Find API to pull data
+2) Aggregate data at game-level using rolling window
